547e1213d77a088935fbcc132edc6b1d68dacdab
Furiiku <github@freakhouse.se>
2024-09-30 13:09:30 +0200

    fix: use mutationobserver to find fav store list

--- src/renderers/favstores.ts
+++ src/renderers/favstores.ts
@@ -6,32 +6,55 @@ async function _clearAllStores (event: MouseEvent): Promise<void> {
   location.reload()
 }
 
-export const renderClearFavoriteStoresUtility = (): void => {
-  const ul = document.querySelector('.list-group')
+function observeDOM (): void {
+  const targetNode = document.body
 
-  if (ul) {
-    const li = document.createElement('li')
-    li.className = 'list-group-item store-list-item'
-    const label = document.createElement('label')
-    label.className = 'stock-favorite'
-    label.title = 'Rensa'
-    const checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-    const span1 = document.createElement('span')
-    span1.textContent = 'Rensa alla butiker'
-    const link = document.createElement('a')
-    link.href = '#'
-    link.className = 'store-info'
-    link.addEventListener('click', (event) => { _clearAllStores(event).catch(() => { }) })
-    const span2 = document.createElement('span')
-    span2.className = 'store-location'
-    span2.textContent = 'Löser problem me butiker som inte går att ta bort då de försvunnit'
-    link.appendChild(span1)
-    link.appendChild(span2)
-    li.appendChild(label)
-    li.appendChild(link)
-    ul.prepend(li)
-  } else {
-    console.error('Kunde inte hitta UL!')
+  const config = { childList: true, subtree: true }
+
+  const callback = function (mutationsList: MutationRecord[], observer: MutationObserver): void {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        mutation.addedNodes.forEach((node) => {
+          const addedNode = node as unknown as HTMLElement
+          if (addedNode.className && addedNode.className === 'list-group mt-5') {
+            const storelist = addedNode.querySelector('.list-group') as HTMLDivElement
+            if (storelist) {
+              console.log('Found store list', storelist)
+              const li = document.createElement('li')
+              li.className = 'list-group-item store-list-item'
+              const label = document.createElement('label')
+              label.className = 'stock-favorite'
+              label.title = 'Rensa'
+              const checkbox = document.createElement('input')
+              checkbox.type = 'checkbox'
+              const span1 = document.createElement('span')
+              span1.textContent = 'Rensa alla butiker'
+              const link = document.createElement('a')
+              link.href = '#'
+              link.className = 'store-info'
+              link.addEventListener('click', (event) => { _clearAllStores(event).catch(() => { }) })
+              const span2 = document.createElement('span')
+              span2.className = 'store-location'
+              span2.textContent = 'Löser problem me butiker som inte går att ta bort då de försvunnit'
+              link.appendChild(span1)
+              link.appendChild(span2)
+              li.appendChild(label)
+              li.appendChild(link)
+              storelist.prepend(li)
+            }
+          }
+        })
+      }
+    }
   }
+
+  const observer = new MutationObserver(callback)
+  observer.observe(targetNode, config)
+}
+
+let renderedAlready = false
+export const renderClearFavoriteStoresUtility = (): void => {
+  if (renderedAlready) return
+  observeDOM()
+  renderedAlready = true
 }
